7efbfe0e98a45d9d6c9dbe510759caae44ea0ad0
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-11-18 21:13:40 +0100

    more

--- pkg/controllers/message.go
+++ pkg/controllers/message.go
@@ -24,6 +24,44 @@ func CreateMessage(c *fiber.Ctx) error {
 	return c.JSON(&message)
 }
 
+func CreateConversation(c *fiber.Ctx) error {
+	token := utils.UseToken(c)
+	conversation := new(models.Conversation)
+	verifiedID, err := strconv.ParseInt(fmt.Sprintf("%.f", token["UserID"]), 0, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.BodyParser(conversation); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	conversation.UserId = verifiedID
+	db.Save(&conversation)
+	return c.JSON(&conversation)
+}
+
+func GetConversation(c *fiber.Ctx) error {
+	var conversation []models.Conversation
+	token := utils.UseToken(c)
+	ids := c.Params("id")
+	id, err := strconv.ParseInt(ids, 0, 0)
+	if err != nil {
+		panic(err)
+	}
+	verifiedID, err := strconv.ParseInt(fmt.Sprintf("%.f", token["UserID"]), 0, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	if token["IsAdmin"] == true || verifiedID == id {
+		db.Where("reciever_id=?", id).Or("sender_id=?", id).Find(conversation)
+		return c.JSON(&conversation)
+	}
+
+	return c.SendString("you are not an admin")
+}
+
 func GetMessage(c *fiber.Ctx) error {
 	var message []models.Message
 	token := utils.UseToken(c)
@@ -37,7 +75,7 @@ func GetMessage(c *fiber.Ctx) error {
 		panic(err)
 	}
 	if token["IsAdmin"] == true || verifiedID == id {
-		db.Where("reciever_id=?", id).Or("sender_id=?", id).Find(&message)
+		db.Where("conversation_id=?", id).Find(&message)
 		return c.JSON(&message)
 	}
 
--- pkg/models/message.go
+++ pkg/models/message.go
@@ -8,10 +8,24 @@ import (
 
 type Message struct {
 	gorm.Model
-	UserId     int64  `json:"user_id"`
-	Text       string `json:"text"`
-	SenderId   int64  `json:"sender_id"`
-	RecieverId int64  `json:"reciever_id"`
+	ConversationId int64  `json:"conversation_id"`
+	Text           string `json:"text"`
+	SenderId       int64  `json:"sender_id"`
+	Conversation   Conversation
+}
+
+type Conversation struct {
+	gorm.Model
+	UserId     int64 `json:"user_id"`
+	SenderId   int64 `json:"sender_id"`
+	RecieverId int64 `json:"reciever_id"`
+	Message    []Message
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Message{})
 }
 
 func init() {
